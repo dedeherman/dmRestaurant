e4f88ef34d90b3bdd532e507e1191a83420cbaea
Dede Herman <dedehermanvpay@gmail.com>
2021-06-21 19:47:38 +0700

    modified

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
-
-console.log('Hello Coders! :)');
-
-// import './styles/responsive.css';
+import '../styles/responsive.css';
 
 const menu = document.querySelector('#menu');
 const hero = document.querySelector('.hero');
